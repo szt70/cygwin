bef96eeb6b665326f54f261fab8b865701fcb412
shinzato <blackpumpkin.7@gmail.com>
2016-08-14 11:50:39 +0900

    gitGfB^ðterapadÉ

--- script/git_editor.sh
+++ script/git_editor.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 FILE=`cygpath -wa $1`
-cygstart /cygdrive/c/Program\ Files/Sublime\ Text\ 2/sublime_text.exe -c65001 "$FILE"
+#"c:/Program Files/Sublime Text 2/sublime_text.exe" "$FILE"
+#"c:/Program Files (x86)/sakura/sakura.exe" -c65001 "$FILE"
+"c:/Program Files (x86)/TeraPad/TeraPad.exe" "$FILE"
