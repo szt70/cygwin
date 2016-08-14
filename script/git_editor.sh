c4f5472027d76628ba0f8dd99f4fba2c91affe72
shinzato <blackpumpkin.7@gmail.com>
2016-08-14 12:46:43 +0900

    エディタ設定

--- script/git_editor.sh
+++ script/git_editor.sh
@@ -2,4 +2,4 @@
 FILE=`cygpath -wa $1`
 #"c:/Program Files/Sublime Text 2/sublime_text.exe" "$FILE"
 #"c:/Program Files (x86)/sakura/sakura.exe" -c65001 "$FILE"
-"c:/Program Files (x86)/TeraPad/TeraPad.exe" "$FILE"
+"c:/Program Files (x86)/TeraPad/TeraPad.exe" /cu8 "$FILE"
